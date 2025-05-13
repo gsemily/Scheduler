d4f880461f95bd1fdee512b89e899ad7e70f803d
gsemily <gsemily22@gmail.com>
2025-05-14 00:07:20 +0900

    style : add format to created_date, modified_date

--- src/main/java/com/example/scheduler/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/scheduler/dto/ScheduleResponseDto.java
@@ -1,5 +1,7 @@
 package com.example.scheduler.dto;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
@@ -11,6 +13,10 @@ public class ScheduleResponseDto {
     private Long id;
     private String todo;
     private String writer;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime created;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime modified;
 }
